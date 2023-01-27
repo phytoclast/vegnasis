acf511894e2c9f8f6edff2d7c12901fcf2b9e7b3
phytoclast <greg.schmidt@mi.usda.gov>
2023-01-27 12:32:05 -0500

    Adding more functions

--- R/ht.round.R
+++ R/ht.round.R
@@ -3,3 +3,33 @@ ht.round <- function(ht){
   ifelse(ht >= 8,round(ht,0),
          ifelse(ht >= 3, floor(ht*2+0.499)/2,round(ht,1)
          ))}
+
+#This function converts plant height from NASIS to meters.
+
+ht.metric <- function(ft){
+  round(as.numeric(ft)*0.3048,1)
+}
+
+#This function converts plant heights from meters to feet.
+
+ht.medieval <- function(m){
+  round(as.numeric(m)/0.3048,1)
+}
+
+#This function converts tree diameters from NASIS to centimeters.
+
+diam.metric <- function(inch){
+  round(as.numeric(inch)*2.54,0)
+}
+
+#This function converts tree diameters from centimeters to inches.
+
+diam.medieval <- function(cm){
+  round(as.numeric(cm)/2.54,1)
+}
+
+# syns <- read.csv('data/plants/m.ac.csv')
+
+#This function aggregates cover among species within the same stratum or guild assuming random crown distributions, and not exceeding 100%.
+
+cover.agg <- function(x){round(100*(1-10^(sum(log10(1-(x/100.001))))),1)}
