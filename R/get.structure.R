6b42ad508070486d61a12bfb0118af9d45fa9afc
phytoclast <greg.schmidt@mi.usda.gov>
2023-02-13 09:32:24 -0500

    Added more structural categories

--- R/get.structure.R
+++ R/get.structure.R
@@ -21,6 +21,7 @@ x3 <- x3 |> left_join(subset(x2, stratum %in% 'herb')) |> mutate(herb=ifelse(is.
 x3 <- x3 |> left_join(subset(x2, stratum %in% 'moss')) |> mutate(moss=ifelse(is.na(cover),0,cover), stratum=NULL, cover=NULL)
 x3 <- x3 |> left_join(x.ht)
 
+
 x3 <- x3 |> mutate(structure =
                      case_when(tree < 10 ~
                                  case_when(shrub+sapling < 10 ~ 'open grassland',
@@ -36,25 +37,43 @@ x3 <- x3 |> mutate(structure =
                                                               case_when(ht.max < 15 ~ 'open low woodland',
                                                                         ht.max < 30 ~ 'open medium woodland',
                                                                         TRUE ~ 'open high woodland'),
-                                                            TRUE ~ case_when(shrub+sapling < 75 ~
-                                                                               case_when(ht.max < 15 ~ 'open shrubby low woodland',
-                                                                                         ht.max < 30 ~ 'open shrubby medium woodland',
-                                                                                         TRUE ~ 'open shrubby high woodland'),
-                                                                             TRUE ~ case_when(ht.max < 15 ~ 'shrubby low woodland',
-                                                                                              ht.max < 30 ~ 'shrubby medium woodland',
-                                                                                              TRUE ~ 'shrubby high woodland')
+                                                            TRUE ~ case_when(sapling > shrub ~
+                                                                               case_when(shrub+sapling < 75 ~
+                                                                                           case_when(ht.max < 15 ~ 'open regenerating low forest gap',
+                                                                                                     ht.max < 30 ~ 'open regenerating medium forest gap',
+                                                                                                     TRUE ~ 'open regenerating high forest gap'),
+                                                                                         TRUE ~ case_when(ht.max < 15 ~ 'dense regenerating low forest gap',
+                                                                                                          ht.max < 30 ~ 'dense regenerating medium forest gap',
+                                                                                                          TRUE ~ 'dense regenerating high forest gap')),
+                                                                             TRUE ~ case_when(shrub+sapling < 75 ~
+                                                                                                case_when(ht.max < 15 ~ 'open shrubby low woodland',
+                                                                                                          ht.max < 30 ~ 'open shrubby medium woodland',
+                                                                                                          TRUE ~ 'open shrubby high woodland'),
+                                                                                              TRUE ~ case_when(ht.max < 15 ~ 'shrubby low woodland',
+                                                                                                               ht.max < 30 ~ 'shrubby medium woodland',
+                                                                                                               TRUE ~ 'shrubby high woodland'))
                                                             )
                                                   ),
-                                                TRUE ~ case_when(ht.max < 15 ~ 'low forest',
-                                                                 ht.max < 30 ~ 'medium forest',
-                                                                 ht.max < 45 ~ 'high forest',
-                                                                 ht.max < 60 ~ 'tall forest',
-                                                                 TRUE  ~ 'giant forest')
+                                                TRUE ~ case_when(shrub+sapling < 10 ~
+                                                                   case_when(ht.max < 15 ~ 'open low forest',
+                                                                             ht.max < 30 ~ 'open medium forest',
+                                                                             ht.max < 45 ~ 'open high forest',
+                                                                             ht.max < 60 ~ 'open tall forest',
+                                                                             TRUE  ~ 'open giant forest'),
+
+                                                                 TRUE ~ case_when(sapling > shrub ~
+                                                                                    case_when(ht.max < 15 ~ 'advance regen low forest',
+                                                                                              ht.max < 30 ~ 'advance regenmedium forest',
+                                                                                              ht.max < 45 ~ 'advance regenhigh forest',
+                                                                                              ht.max < 60 ~ 'advance regentall forest',
+                                                                                              TRUE  ~ 'giant forest'),
+                                                                                  TRUE ~
+                                                                                    case_when(ht.max < 15 ~ 'shrubby low forest',
+                                                                                              ht.max < 30 ~ 'shrubby medium forest',
+                                                                                              ht.max < 45 ~ 'shrubby high forest',
+                                                                                              ht.max < 60 ~ 'shrubby tall forest',
+                                                                                              TRUE  ~ 'shrubby giant forest')))
+
                                )
                      ))
-
-
-
-
-
 }
