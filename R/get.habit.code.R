2923b8aaf76ba7c2b7c69af59ab47540aa3817dd
phytoclast <greg.schmidt@mi.usda.gov>
2023-02-13 11:31:03 -0500

    Renamed function, and created new function to fill missing NASIS life form "type".

--- R/get.habit.code.R
+++ R/get.habit.code.R
@@ -17,11 +17,8 @@ get.habit.code <- function(taxa){
 
 
 
-
-
-
 #Lookup the growth habit descriptive name or ESIS  from a vector of growth habit codes ----
-get.habit <- function(code,type='name'){
+get.habit.name <- function(code,type='name'){
   x  <-  as.data.frame(cbind(code=code)) |>
     left_join(gho, by = c('code'='Revised.Symbol'), multiple = 'first')
   if(type %in% 'stem'){return(x$First)}else if(type %in% 'ESIS'){return(x$ESIS.Group)}else{return(x$Habitname)}
@@ -44,3 +41,37 @@ get.ht.max <- function(taxa){
   x <- x |> mutate(ht0 = ifelse(is.na(ht0)| ht0 %in% "", ht.max, as.character(ht0)))
   x <- x[,1:2]
   return(as.numeric(x$ht0)) }
+
+
+
+#Lookup the NASIS life form "type" according to default growth habit of each taxon. Include column of existing types if existing types are to be preserved while filling in only missing values.
+
+fill.type <- function(taxa, type=NA){
+  x  <-  data.frame(taxa=taxa, type = type) |> mutate(GH0 = '', genus = str_split_fixed(taxa , '[[:blank:]]',3)[,1])
+  #first try straight join ----
+  x <- x |> left_join(taxon.habits[,c('Scientific.Name','GH')], by = c('taxa'='Scientific.Name'), multiple = 'first')
+  x <- x |> mutate(GH0 = ifelse(is.na(GH0)| GH0 %in% "", GH, as.character(GH0)))
+  x <- x[,1:4]
+  #then try synonym join ----
+  x <- x |> left_join(syns[,c('acc','syn')], by=c('taxa'='syn'), multiple = 'first') |> left_join(taxon.habits[,c('Scientific.Name','GH')], by = c('acc'='Scientific.Name'), multiple = 'first')
+  x <- x |> mutate(GH0 = ifelse(is.na(GH0)| GH0 %in% "", GH, as.character(GH0)))
+  x <- x[,1:4]
+  #finally try genus only ----
+  x <- x |> left_join(genus.habits, by = c('genus'='genus'), multiple = 'first')
+  x <- x |> mutate(GH0 = ifelse(is.na(GH0)| GH0 %in% "", GH, as.character(GH0)))
+  x <- x[,1:3]
+  x <- x |> mutate(nasis = case_when(
+    grepl('^T', GH0) ~ 'tree',
+    grepl('^S', GH0)| grepl('^L', GH0) | grepl('^E', GH0) ~ 'shrub/vine',
+    grepl('^H.G', GH0)~ 'grass/grasslike',
+    grepl('^H', GH0)~ 'forb',
+    grepl('^N.B', GH0)~ 'moss',
+    grepl('^N.L', GH0)~ 'lichen',
+    grepl('^N', GH0)~ 'microbiotic crust',
+    TRUE ~ NA
+  ))
+  nasis <- ifelse(is.na(x$type), x$nasis, x$type)
+  return(nasis) }
+
+
+
