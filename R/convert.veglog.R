42b5be75c75f98e26706d6049413b19f5613ac85
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2023-02-15 21:40:57 -0500

    Start creating function to convert personal database

---
+++ R/convert.veglog.R
@@ -0,0 +1,10 @@
+#convert Text export from VegLog MS Access Database
+# library(vegnasis)
+# veg.raw <- vegnasis::nasis.veg
+#
+# obss <- obs |> mutate(date = paste0(Year,"-", Mon,"-",  Day))
+# obss <- obsspp |> left_join(obs[obs$Day paste0('Year',"-", 'Mon',"-",  'Day'), Observation_ID, User_Plot_ID)
+#
+# obss <- obs |> mutate(date = as.Date(ISOdate(year = Year,
+#         month = Mon,
+#         day = Day)))
