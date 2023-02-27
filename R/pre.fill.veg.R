318b35f1e0ac0079b44dc473798c3392c38ce53d
phytoclast <greg.schmidt@mi.usda.gov>
2023-02-27 14:35:34 -0500

    Generic data cleaning function for imported data.

---
+++ R/pre.fill.veg.R
@@ -0,0 +1,24 @@
+#This function retains standardized columns, removes non-standard columns, and establishes missing standardized columns with missing data.
+
+pre.fill.veg <- function(x){
+  if(!'plot' %in% colnames(x)){x$plot=NA_character_}
+  if(!'symbol' %in% colnames(x)){x$symbol=NA_character_}
+  if(!'taxon' %in% colnames(x)){x$taxon=NA_character_}
+  if(!'type' %in% colnames(x)){x$type=NA_character_}
+  if(!'nativity' %in% colnames(x)){x$nativity=NA_character_}
+  if(!'cover' %in% colnames(x)){x$cover=NA_real_}
+  if(!'stratum.min' %in% colnames(x)){x$stratum.min=NA_real_}
+  if(!'stratum.max' %in% colnames(x)){x$stratum.max=NA_real_}
+  if(!'crown.min' %in% colnames(x)){x$crown.min=NA_real_}
+  if(!'crown.max' %in% colnames(x)){x$crown.max=NA_real_}
+  if(!'diam.min' %in% colnames(x)){x$diam.min=NA_real_}
+  if(!'diam.max' %in% colnames(x)){x$diam.max=NA_real_}
+  if(!'diam' %in% colnames(x)){x$diam=NA_real_}
+  if(!'BA' %in% colnames(x)){x$BA=NA_real_}
+
+    x <- x %>% subset(select= c("plot","symbol","taxon","type",
+                                "nativity","cover","stratum.min","stratum.max","crown.min","crown.max","diam.min","diam.max",'diam',"BA"))
+  return(x)
+}
+
+
