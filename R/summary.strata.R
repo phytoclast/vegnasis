2de23f6eae78e0782376a0c9c9fd36d76901cd94
phytoclast <greg.schmidt@mi.usda.gov>
2023-03-20 16:16:41 -0400

    Add top and bottom heights, and fill zeros for missing structure

--- R/summary.strata.R
+++ R/summary.strata.R
@@ -8,15 +8,15 @@ summary.strata <-  function(x, breaks=c(0.5,5,15)){
     y0 <- x %>% subset(ht.max < brks[i+1] & ht.max >= brks[i])
 
     if(nrow(y0)>0){
-      y0 <- y0 %>% mutate(stratum=i, stratum.label = paste0(brks[i], "-", ifelse(i==nbks, "+",brks[i+1])))
-      y1 <- y0 %>% group_by(plot, type, stratum, stratum.label) %>% summarise(Cover = cover.agg(cover))
+      y0 <- y0 %>% mutate(stratum=i, stratum.label = paste0(brks[i], "-", ifelse(i==nbks, "+",brks[i+1])), bottom= brks[i], top = ifelse(i==nbks,brks[i]+1,brks[i+1]))
+      y1 <- y0 %>% group_by(plot, type, stratum, stratum.label, bottom, top) %>% summarise(Cover = cover.agg(cover))
 
     if(is.null(y)){y <- y1}else{y <- rbind(y, y1)}}
   }
   return(y)
 }
 
-#This function summarises crown overlap by stratum and growth habit. Inputs require a data frame processed by clean.veg and  user supplied vector of stratum height breaks. ----
+#This function summarize crown overlap by stratum and growth habit. Inputs require a data frame processed by clean.veg and  user supplied vector of stratum height breaks. ----
 breaks <- c(0.1, 0.5, 2, 5, 10, 20, 30)
 summary.crown.thickness <-  function(x, breaks=c(0.5,5,15)){
   y <- NULL
@@ -26,10 +26,20 @@ summary.crown.thickness <-  function(x, breaks=c(0.5,5,15)){
     y0 <- x %>% subset(ht.min < brks[i+1] & ht.max >= brks[i])
 
     if(nrow(y0)>0){
-      y0 <- y0 %>% mutate(stratum=i, stratum.label = paste0(brks[i], "-", ifelse(i==nbks, "+",brks[i+1])))
-      y1 <- y0 %>% group_by(plot, type, stratum, stratum.label) %>% summarise(Cover = cover.agg(cover))
+      y0 <- y0 %>% mutate(stratum=i, stratum.label = paste0(brks[i], "-", ifelse(i==nbks, "+",brks[i+1])), bottom= brks[i], top = ifelse(i==nbks,brks[i]+1,brks[i+1]))
+      y1 <- y0 %>% group_by(plot, type, stratum, stratum.label, bottom, top) %>% summarise(Cover = cover.agg(cover))
 
     if(is.null(y)){y <- y1}else{y <- rbind(y, y1)}}
   }
   return(y)
 }
+
+#This function adds zeros to plots lacking cover values found in other plots ----
+
+structure.fill.zero <- function(x){
+  x.plot <- unique(subset(x, select=c("plot")))
+  x.mid <- unique(subset(x, select=c("type","stratum","stratum.label","bottom","top")))
+  x.fill <- merge(x.plot, x.mid) |> mutate(Cover = 0)
+  x.fill <- rbind(x, x.fill)
+  x.fill <- x.fill |> group_by(plot,type,stratum,stratum.label,bottom,top) |> summarise(Cover = max(Cover))
+  return(x.fill)}
