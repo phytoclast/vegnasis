26acc2c49f134b7098801f0b2db3d19e3db4ab8b
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2023-04-13 21:36:04 -0400

    Add normalize rare species and fix bug that prevented previous rc=FALSE option from actually working.

--- R/make.plot.matrix.R
+++ R/make.plot.matrix.R
@@ -7,11 +7,12 @@
 # x = veg
 # This function takes a standardized vegetation plot dataframe and converts it to a community matrix that can be converted to a distance matrix. Parameters include options for transformations and whether to use absolute or relative cover. Choices for transformation include: "log" for log base 10, "sqrt" for square root, and nothing for no transformation.
 
-make.plot.matrix <- function(x, tr = NA, rc = TRUE){
+make.plot.matrix <- function(x, tr = NA, rc = TRUE, nr=FALSE){
 
 x <- x |> group_by(plot, taxon) |> summarise(cover = cover.agg(cover))
-if(rc){x <- x |> group_by(plot) |> mutate(relcover= cover/(sum(cover)*100+0.0000001))}
-df <- data.frame(r=x$plot, c=x$taxon, v=x$relcover) |> subset(nchar(r) >= 1 & nchar(c) >= 1)
+if(rc){x <- x |> group_by(plot) |> mutate(cover= cover/(sum(cover)*100+0.0000001))}
+if(nr){x <- x |> group_by(taxon) |> mutate(cover= cover/(max(cover)*100+0.0000001))}
+df <- data.frame(r=x$plot, c=x$taxon, v=x$cover) |> subset(nchar(r) >= 1 & nchar(c) >= 1)
 if(tr %in% 'log'){
   df$v <- (log10(df$v+0.1)+1)/(log10(100+0.1)+1)*100
 }else if(tr %in% 'sqrt'){
