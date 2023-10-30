cca05b76c8bb393be43b3999df311fe46031878d
phytoclast <greg.schmidt@mi.usda.gov>
2023-10-30 16:10:53 -0400

    Add ability to work in extra USFS stratum. Also add date and lan/lon to standard schema.

--- R/convert.veglog.R
+++ R/convert.veglog.R
@@ -9,28 +9,35 @@ clean.veg.log <- function(obssites, obstaxa){
                                                     month = Mon,
                                                     day = Day))) |> subset(Year >2001)
 
-  obss <- obss |> select(Observation_ID, Observation_Label,Date,Latitude,Longitude,f3,s3,t2,Lichen_Cover,Moss_Cover,Herb_Cover,Shrub_Cover,Subcanopy_Cover,Tree_Cover,BA_Factor)
-  colnames(obss) <- c("Observation_ID","Observation_Label", "Date","Latitude","Longitude","fieldupper","shrubupper","subcanopyupper", "Lichen", "Bryophyte", "Field.agg", "Shrub.agg","Subcanopy.agg","Tree.agg","BA_Factor")
+  obss <- obss |> select(Observation_ID, Observation_Label,Date,Latitude,Longitude,f3,s3,t2,Lichen_Cover,Moss_Cover,Herb_Cover,Shrub_Cover,TallShrub_Cover,Subcanopy_Cover,Tree_Cover,BA_Factor)
+  colnames(obss) <- c("Observation_ID","Observation_Label", "Date","Latitude","Longitude","fieldupper","shrubupper","subcanopyupper", "Lichen", "Bryophyte", "Field.agg", "Shrub.agg","TallShrub.agg","Subcanopy.agg","Tree.agg","BA_Factor")
 
-  obst <- obstaxa |> select(Observation_ID, AcTaxon, Habit, Field, Shrub, Subcanopy, Tree, Fmin,Fmax,Smin,Smax,SCmin,SCmax,Tmin,Tmax,Dmin,Dmax,BA)
+  obst <- obstaxa |> select(Observation_ID, AcTaxon, Habit, Field, Shrub, TallShrub, Subcanopy, Tree, Fmin,Fmax,Smin,TSmin, TSmax, Smax,SCmin,SCmax,Tmin,Tmax,Dmin,Dmax,BA)
+
 
 
   x <- obss |> left_join(obst, by=c('Observation_ID'='Observation_ID'), multiple='all')
+  #determine whether a tall shrub stratum should exist
+  x <- x |> group_by(Observation_ID) |> mutate(shrubupper = ifelse(!is.na(TallShrub.agg) | (!is.na(TallShrub) & sum(TallShrub) > 0), 2,5))
   field <- x |> mutate(cover=Field, stratum.min = 0, stratum.max = fieldupper, crown.min = Fmin, crown.max = Fmax, BA = NA_real_, Dmin = NA_real_, Dmax = NA_real_)
+
   shrub <- x |> mutate(cover=Shrub, stratum.min = fieldupper, stratum.max = shrubupper, crown.min = Smin, crown.max = Smax, BA = NA_real_, Dmin = NA_real_, Dmax = NA_real_)
+  tallshrub <- x |> mutate(cover=TallShrub, stratum.min = 2, stratum.max = 5, crown.min = TSmin, crown.max = TSmax, BA = NA_real_, Dmin = NA_real_, Dmax = NA_real_)
   subcan <- x |> mutate(cover=Subcanopy, stratum.min = shrubupper, stratum.max = subcanopyupper, crown.min = SCmin, crown.max = SCmax, BA = ifelse(Tree == 0 & Subcanopy > 0, tree.ct.BA(BA, BA_Factor),NA_real_), Dmin = ifelse(Tree == 0 & Subcanopy > 0, Dmin, NA_real_), Dmax = ifelse(Tree == 0 & Subcanopy > 0, Dmax, NA_real_))
   tree <- x |> mutate(cover=Tree, stratum.min = subcanopyupper, stratum.max = NA_real_, crown.min = Tmin, crown.max = Tmax, BA = ifelse(Tree > 0, tree.ct.BA(BA, BA_Factor),NA_real_), Dmin = ifelse(Tree > 0, Dmin, NA_real_), Dmax = ifelse(Tree > 0, Dmax, NA_real_))
 
-  x <- rbind(field, shrub, subcan, tree) |> select(Observation_ID, Observation_Label, AcTaxon, cover, stratum.min, stratum.max, crown.min, crown.max, Dmin, Dmax, BA)
+  x <- rbind(field, shrub, tallshrub, subcan, tree) |> select(Observation_ID, Observation_Label, Date, Latitude, Longitude, AcTaxon, cover, stratum.min, stratum.max, crown.min, crown.max, Dmin, Dmax, BA)
 
 
-  colnames(x) <- c('plot', 'label', 'taxon', 'cover', 'stratum.min', 'stratum.max', 'crown.min', 'crown.max', 'dbh.min', 'dbh.max', 'BA')
+  colnames(x) <- c('plot', 'label', 'date', 'lat', 'lon', 'taxon', 'cover', 'stratum.min', 'stratum.max', 'crown.min', 'crown.max', 'dbh.min', 'dbh.max', 'BA')
   x <- x |> mutate(symbol=NA, type=NA, nativity=NA,
                    cover = ifelse(cover > 100, 100, cover),
                    # habit = NA_character_,
                    BA = ifelse(BA <= 0,NA_real_,BA),
-                   )|>
+  )|>
     subset(cover > 0)|>
-    select(plot, label, symbol, taxon, type, nativity, cover, stratum.min, stratum.max, crown.min, crown.max, dbh.min, dbh.max, BA)
+    select(plot, label, date, lat, lon, taxon, type, nativity, cover, stratum.min, stratum.max, crown.min, crown.max, dbh.min, dbh.max, BA)
+  # x <-  x |> fill.nativity.df() |> fill.type.df() |> mutate(symbol = fill.usda.symbols(taxon))
+
   return(x)
 }
