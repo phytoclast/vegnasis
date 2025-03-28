50cb49b1b4e1ff9e08ea604ebcc38b78883b991e
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2025-03-27 21:55:17 -0400

    Update concave hull with more control of curve depth.

--- R/complextrees.R
+++ R/complextrees.R
@@ -340,7 +340,108 @@ makeCrownShape <- function(ht.max=5, ht.min=1, crwd=2, dbh=0.3, tip=0.01, crshap
   return(shapes)
 }
 
+#' Minimum width of convex hull
+#'
+#' @param x vector of x coordinates
+#' @param y vector of y coordinates
+#'
+#' @returns Width of convex hull.
+#' @export
+#'
+#' @examples
+minXY <- function(x,y){
+  df <- data.frame(x=x,y=y)
+  df22 <- rotate(x=df$x, y=df$y, a=22.5, cx = 0, cy = 0)
+  df45 <- rotate(x=df$x, y=df$y, a=45, cx = 0, cy = 0)
+  df67 <- rotate(x=df$x, y=df$y, a=67.5, cx = 0, cy = 0)
+  x <- pmin(max(df$x) - min(df$x),
+            max(df$y) - min(df$y),
+            max(df22$x) - min(df22$x),
+            max(df22$y) - min(df22$y),
+            max(df45$x) - min(df45$x),
+            max(df45$y) - min(df45$y),
+            max(df67$x) - min(df67$x),
+            max(df67$y) - min(df67$y))
+  return(x)}
 
+
+
+#' Buffer around or within convex hull.
+#'
+#' @param x vector of x coordinates
+#' @param y vector of y coordinates
+#' @param s vector of integers determining the ordering of coordinates.
+#' @param b Buffer thickness (use negative number for internal buffer)
+#'
+#' @returns Data frame of xy coordinates of densified points following the border of hull.
+#' @export
+#'
+#' @examples
+hull.buffer <- function(x, y, s, b){
+  df <- data.frame(x=x,y=y,s=s)
+  df <- subset(df, !is.na(s))
+  df <- mutate(df,nx=NA,ny=NA)
+  #ensure that negative buffer doesn't go pass minimum thickness
+  mxy <- minXY(x=x,y=y)
+  f = ifelse(b < 0, -1*pmin(abs(b),mxy/2),b)
+  smax <- max(df$s, na.rm = TRUE)
+  #create inner border to contain convexity
+  for(i in 1:smax){#i=5
+    i0 = ifelse(i == 1,smax,i-1)
+    i2 = ifelse(i == smax,1,i+1)
+    x0 <- df[df$s %in% i0,]$x
+    y0 <- df[df$s %in% i0,]$y
+    x1 <- df[df$s %in% i,]$x
+    y1 <- df[df$s %in% i,]$y
+    x2 <- df[df$s %in% i2,]$x
+    y2 <- df[df$s %in% i2,]$y
+    l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
+    l2 <- ((x1-x2)^2+(y1-y2)^2)^0.5
+
+    a0 = acos((x1-x0)/l0)
+    a0 = ifelse(y1 - y0 >=0,a0,-1*a0)
+    dfr <- vegnasis::rotate(x=x1, y=y1, a=a0/2/pi*360, cx=x0, cy=y0)
+    xr1 <- dfr$x[1]
+    yr1 <- dfr$y[1]
+    dfr <- vegnasis::rotate(x=x2, y=y2, a=a0/2/pi*360, cx=x0, cy=y0)
+    xr2 <- dfr$x
+    yr2 <- dfr$y
+    a2 <- acos((xr2-xr1)/l2)
+    a2 <- ifelse(yr2 - yr1 >=0,a2,-1*a2)
+    a3 <- pi-(pi-a2)/2
+    yr3 <- f*sin(a3)+yr1
+    xr3 <- f*cos(a3)+xr1
+    dfr2 <- vegnasis::rotate(x=xr3, y=yr3, a=-a0/2/pi*360, cx=x0, cy=y0)
+    nnx <- dfr2$x[1]
+    nny <- dfr2$y[1]
+    df$nx <- ifelse(df$s %in% i, nnx,df$nx)
+    df$ny <- ifelse(df$s %in% i, nny,df$ny)
+  }
+  #insert new points on inner border
+
+  for(i in 1:nrow(df)){#i=3
+    i0 = ifelse(i == 1,smax,i-1)
+    x0 <- df[df$s %in% (i0),]$nx
+    y0 <- df[df$s %in% (i0),]$ny
+    x1 <- df[df$s %in% i,]$nx
+    y1 <- df[df$s %in% i,]$ny
+    l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
+    nseg <- pmin(10,pmax(5,5*floor(3*l0/mxy)))
+    p0 <- c(0:(nseg-1))/nseg
+    p1 <- 1-p0
+    newX0 <- x0*p0+x1*p1
+    newY0 <- y0*p0+y1*p1
+    if(i==1){
+      newY <- newY0
+      newX <- newX0
+    }else{
+      newY <- c(newY,newY0)
+      newX <- c(newX,newX0)
+    }
+  }
+  df1 <- data.frame(x=newX, y=newY)
+  return(df1)
+}
 
 #' Concave Hull
 #'
@@ -348,7 +449,9 @@ makeCrownShape <- function(ht.max=5, ht.min=1, crwd=2, dbh=0.3, tip=0.01, crshap
 #' @param y Vector of x coordinates.
 #' @param concavity Degrees of concavity: 0 = convex, 1 = first order, 2 = second order...
 #' @param curvy Adds more vertices between truce vertices for a smoother border.
-#' @param mag Magnitude of curve (search deeper for vertices), representing proportion relative to distance between convex vertices.
+#' @param maxdepth Maximum depth of concave curve.
+#' @param minspan Minimum distance between vertices for concavity.
+#' @param mag Magnitude of search for deeper vertices, representing proportion relative to distance between convex vertices.
 #' @param deep Retain vertices of curve even when no vertices are found.
 #'
 #' @returns Vector of xy points with appropriate ordering to create an outline around a set of input xy points.
@@ -370,8 +473,9 @@ makeCrownShape <- function(ht.max=5, ht.min=1, crwd=2, dbh=0.3, tip=0.01, crshap
 #' @examples geom_point(data=df,aes(x=x,y=y))+
 #' @examples coord_fixed()
 
-cavhull <- function(x,y, concavity = 0, curvy = FALSE, mag = 1, deep=FALSE){
+cavhull <- function(x,y, concavity = 0, curvy = FALSE, maxdepth=NA, minspan=0, mag = 1){
   n=5 #number of segments to search between convex faces
+
   df <- data.frame(x=floor(x*1000)/1000,y=floor(y*1000)/1000) |> unique()
 
   #convex hull ----
@@ -417,19 +521,27 @@ cavhull <- function(x,y, concavity = 0, curvy = FALSE, mag = 1, deep=FALSE){
   }
   #concave hull first degree ----
   df <- df |> mutate(s1 = s, type = 'core')
-
+  #deep curve
+  if(TRUE){
+    if(is.na(maxdepth)){maxdepth=-1*minXY(x=df$x,y=df$y)/2}else{maxdepth=-1*maxdepth}
+    buff0 <- hull.buffer(df$x, df$y, df$s, b=maxdepth)
+    buff <- data.frame(x=buff0$x, y=buff0$y, s=NA,l1=NA,a1=NA,xr=NA,
+                       yr=NA,h=NA,s1=NA, type='core1')
+    df <- df |> rbind(rbind(buff))
+  }
   if(concavity > 0){
     for(k in 1:concavity){ #k=1
       smax <- max(df$s, na.rm = TRUE)
       #visit each convex hull boundary and rotate to a common reference
       for(i in 1:smax){#i=2
-        if(l0 > 0){
-          i0 = ifelse(i == 1,smax,i-1)
-          x0 <- df[df$s %in% (i0),]$x
-          y0 <- df[df$s %in% (i0),]$y
-          x1 <- df[df$s %in% i,]$x
-          y1 <- df[df$s %in% i,]$y
-          l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
+
+        i0 = ifelse(i == 1,smax,i-1)
+        x0 <- df[df$s %in% (i0),]$x
+        y0 <- df[df$s %in% (i0),]$y
+        x1 <- df[df$s %in% i,]$x
+        y1 <- df[df$s %in% i,]$y
+        l0 <- ((x1-x0)^2+(y1-y0)^2)^0.5
+        if(l0 > minspan){
           a0 = acos((x1-x0)/l0)
           a0 = ifelse(y1 - y0 >=0,a0,-1*a0)
           dfr <- vegnasis::rotate(x=df$x, y=df$y, a=a0/2/pi*360, cx=x0, cy=y0)
@@ -438,17 +550,7 @@ cavhull <- function(x,y, concavity = 0, curvy = FALSE, mag = 1, deep=FALSE){
                              xs=NA,xa=NA,ys=NA,yl0=NA,ydiff=NA,microinc=NA)
           #use wave to select closest concave points
           en <- pmax(3,floor(pmin(n,l0/5)))*3
-          #deep curve
-          if(deep & k == concavity){
-            wave0 <- data.frame(x=(0:(en+1))/(en+1))
-            wave0 <- wave0 |> mutate(a=x*2*pi,y=(cos(a)^1-1)/2*mag)
-            wave <- data.frame(x=NA, y=NA, s=NA,l1=NA,a1=NA,xr=wave0$x*l0,
-                               yr=wave0$y*l0,h=NA,s1=NA, type='cave',
-                               xs=NA,xa=NA,ys=NA,yl0=NA,ydiff=NA,microinc=NA)
-            wavr <- vegnasis::rotate(x=wave$xr, y=wave$yr, a=-a0/2/pi*360, cx=0,cy=0)
-            wave <- wave |> mutate(x=wavr$x+x0,y=wavr$y+y0) |> subset(!yr >=0)
-            df <- df |> rbind(rbind(wave))
-          }
+
           df <- df |> mutate(xs = xr/l0, xa = xs*2*pi, ys = (cos(xa)^1-1)/2*mag,
                              yl0 = (yr/l0), ydiff = yl0-ys)
           curmax <- max(subset(df, xs > 0 & xs < 1)$ydiff)
@@ -479,7 +581,7 @@ cavhull <- function(x,y, concavity = 0, curvy = FALSE, mag = 1, deep=FALSE){
           df$microinc <- renumber(df$microinc)
 
           df <- df |> mutate(s1 = ifelse(!is.na(microinc) & is.na(s), i0+microinc/1000,s1))
-          df <- df |> subset(type %in% 'core' | !is.na(s1))
+          df <- df |> subset(type %in% c('core','core1') | !is.na(s1))
         }}
       df <- df |> mutate(s = renumber(s1), s1 = s)
     }
