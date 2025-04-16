75792817b121bebd8ee61a07fe207ed1d1de51f2
Greg Schmidt <greg.schmidt@mi.usda.gov>
2025-04-16 11:59:30 -0400

    Added features to clean.veg() to allow preemptive filling of plant type to facilitate assignment of stratum when height values are missing. Also add option to fill in missing heights.

--- R/clean.veg.R
+++ R/clean.veg.R
@@ -1,7 +1,60 @@
 #This function takes veg plot imported from NASIS (veg.spp <- soilDB::get_vegplot_species_from_NASIS_db()) and consolidates the redundant cover and height fields, and fills in missing heights.
+#' Clean vegetation data imported from NASIS
+#'
+#' @description
+#' This function takes veg plot imported from NASIS and consolidates the redundant cover and height fields.
+#'
+#'
+#' @param x Species composition data frame derived from a soilDB NASIS import function.
+#' @param type Should missing plant types (habits) be filled (TRUE/FALSE)?
+#' @param hts Should missing plant heights be filled to allow assignment to strata (TRUE/FALSE)?
+#'
+#' @return Data frame containing a standardized set of quanitative vegetation parameters.
+#' @export
+#'
+#' @examples veg.raw <- soilDB::get_vegplot_species_from_NASIS_db()
+#' @examples veg <- clean.veg(veg.raw)
+#'
+clean.veg <- function(x, type=TRUE, hts=FALSE){
+  if(type){
+    x <- x |> mutate(planttypegroup = fill.type(plantsciname, type=planttypegroup))
+    x <- x |> mutate(planttypegroup = case_when(
+      is.na(planttypegroup) & plantsym %in% c("2CYAN","2ALGA","2AB","2AFW","2AG","2AM","2AR") ~ 'microbiotic crust',
+      is.na(planttypegroup) & plantsym %in% c("2LICHN","2LC","2LCB","2LCE","2LCN","2LCGEL","2LCGON",
+                                              "2LCP","2LCQ","2LCS","2LF","2LFL","2LFU","2LUV","2LU",
+                                              "2LUF","2LUR") ~ 'lichen',
+      is.na(planttypegroup) & plantsym %in% c("2BRY","2MOSS","2HORN",
+                                              "2LW","2LWL","2LWT") ~ 'moss',
+      is.na(planttypegroup) & plantsym %in% c("2FERN","2FORB","2FA","2FB","2FD",
+                                              "2FDA","2FDB","2FDP","2FI","2FM","2FMA","2FMB",
+                                              "2FMP","2FN","2FP","2FS","2FSA","2FSB","2FSP",
+                                              "2VH","2VHA","2VHD","2VHDA","2VHDP","2VHM","2VHMA",
+                                              "2VHMP","2VHP","2VHS","2VHSA","2VHSP","2VW") ~ 'forb',
+      is.na(planttypegroup) & plantsym %in% c("2GRAM","2GA","2GB","2GI","2GN","2GP","2GW","2GL",
+                                              "2GLA","2GLB","2GLP") ~ 'grass/grasslike',
+      is.na(planttypegroup) & plantsym %in% c("2SHRUB","2SB","2SD","2SDB","2SDBD","2SDBM",
+                                              "2SDN","2SE","2SEB","2SEBD","2SEBM","2SEN",
+                                              "2SN","2S","2SS","2SSL","2SSS","2SUBS","2SSB",
+                                              "2SSD","2SSDB","2SSDBD","2SSDBM","2SSDN",
+                                              "2SSE","2SSEB","2SSEBD","2SSEBM","2SSEN",
+                                              "2SSN","2SSS2","2SSSL","2SSSS",
+                                              "2VWD","2VWDD","2VWDM","2VWE","2VWED","2VWEM") ~ 'shrub/vine',
+      is.na(planttypegroup) & plantsym %in% c("2TREE","2TB","2TD","2TDB","2TDBD","2TDBM",
+                                              "2TDN","2TE","2TEB","2TEBD","2TEBM","2TEN",
+                                              "2TN","2TS","2TSL","2TSS") ~ 'tree',
+      TRUE ~ planttypegroup))
+  }
 
-clean.veg <- function(x){
   x <- x %>% mutate(
+    vegetationstratalevel =
+      case_when(
+        is.na(vegetationstratalevel) & is.na(akstratumcoverclass) & is.na(livecanopyhttop) &
+          is.na(plantheightclupperlimit) & (!is.na(understorygrcovclass)|!is.na(understorygrcovpct)) &
+          (is.na(speciescancovclass) & is.na(speciescancovpct))  ~ 'understory',
+        is.na(vegetationstratalevel) & is.na(akstratumcoverclass) & is.na(livecanopyhttop) &
+          is.na(plantheightclupperlimit) & planttypegroup %in% 'tree'  ~ 'overstory',
+        TRUE ~ vegetationstratalevel),
+
     cover = case_when(
       !is.na(akstratumcoverclasspct) ~ as.numeric(akstratumcoverclasspct),
       !is.na(speciescancovpct) ~ as.numeric(speciescancovpct) + ifelse(speciestraceamtflag,0.2,0),
@@ -83,10 +136,15 @@ clean.veg <- function(x){
 
   x <- x %>% subset(select= c("plot","label","date","symbol","taxon","type",
                               "nativity","cover","stratum.min","stratum.max","crown.min","crown.max","dbh.min","dbh.max","BA"))
+
+  if(hts){
+    x <- fill.hts.df(x)
+  }
+
   return(x)
 }
 
-#This function takes veg plot transect inported from NASIS (veg.spp <- soilDB::get_vegplot_species_from_NASIS_db()) and consolidates the redundant cover and height fields, and fills in missing heights.
+#This function takes veg plot transect imported from NASIS (veg.spp <- soilDB::get_vegplot_species_from_NASIS_db()) and consolidates the redundant cover and height fields, and fills in missing heights.
 
 clean.veg.transect <- function(x){
   x <- x %>% mutate(
@@ -110,10 +168,10 @@ clean.veg.transect <- function(x){
     foliar = case_when(!is.na(speciesfoliarcovhitcount) & !is.na(speciestotfoliarcovlineint) ~ as.numeric(speciesfoliarcovhitcount/speciestotfoliarcovlineint)*100,
                        !is.na(speciesfoliarcovpctlineint) ~ as.numeric(speciesfoliarcovpctlineint),
                        !is.na(speciescomppctlineintercept) ~ as.numeric(speciescomppctlineintercept),
-                        TRUE ~ NA_real_),
+                       TRUE ~ NA_real_),
 
     biomass = case_when(!is.na(speciesaveyielddblsamp) ~ biomass.metric(speciesaveyielddblsamp),
-                         TRUE ~ NA_real_),
+                        TRUE ~ NA_real_),
     rbiomass = case_when(!is.na(speciescomppctdblsamp) ~ as.numeric(speciescomppctdblsamp),
                          TRUE ~ NA_real_),
 
