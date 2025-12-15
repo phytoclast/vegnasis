cd1be730d06b046016e2b1aac59eec42aef09aea
Greg Schmidt <gregoryjohnschmidt@gmail.com>
2025-12-15 16:40:41 -0500

    Update dominance.R

--- data_raw/dominance.R
+++ data_raw/dominance.R
@@ -31,3 +31,43 @@ sumdomspp <- domspp  |>   group_by(stratum) |>   summarise(mindom = min(ndom, na
 mcover <- veg2 |> group_by(rnk) |> summarise(mcov = mean(rcover))
 
 ddrop <- veg2 |> subset(cumis50 %in% c(1,2)) |>  group_by(plot, stratum) |> summarise(mxcov = max(rcover), mncov = min(rcover), ddrp = mncov/mxcov) |> ungroup() |> summarise(ddrp2 = mean(ddrp))
+
+
+#####attempt function ----
+taxon=veg$taxon
+cover=veg$cover
+ht.max=veg$ht.max
+type = veg$type
+veg <- veg
+isdominant <-  function(veg=NULL, taxon, cover, ht.max, type){
+  if(is.null(veg)){
+    veg <- data.frame(taxon=taxon,cover=cover, ht.max=ht.max, type=type)
+  }
+
+  group_taxa <- c(group_vars(veg),'stratum','taxon')
+  group_strat <- c(group_vars(veg),'stratum')
+  group_arrange <- c(group_vars(veg),'stratum','-cover')
+  joincol <- c(group_vars(veg),"stratum","taxon","isdom")
+
+  veg <- veg |>
+    mutate(stratum = case_when(ht.max > 5 ~ 'tree',
+                               ht.max > 0.5 & type %in% c('tree','shrub/vine') ~ 'shrub',
+                               TRUE ~ 'herb'))
+  veg2 <- veg |>
+    group_by(across(all_of(group_taxa))) |> summarise(ht.max = weighted.mean(ht.max,cover), cover = cover.agg(cover), type=first(type)) |> ungroup()
+
+  veg2 <- veg2 |> group_by(across(all_of(group_strat))) |> mutate(stratumcover = cover.agg(cover), stratumsum = sum(cover),  nsp = length(cover)) |> ungroup()
+
+  veg2 <- veg2 |> group_by(across(all_of(group_strat))) |> arrange(-cover) |> mutate(rcover = (cover)/stratumsum, cumtotal = cumsum(cover)/stratumsum, is50 = ifelse(cumtotal >=0.5,1,0), cumis50 =cumsum(is50), rnk = rank(-rcover, ties.method = 'first')) |> ungroup() |> mutate(isdom = ifelse(stratumcover >= 5 & ((cumis50 <= 1 & rnk <=10) | (cover >= 20 & rnk <=5)),1,0))
+
+  veg <- veg |> left_join(veg2[,joincol])
+  return(veg$isdom)
+
+}
+
+
+
+
+veg <- veg.raw |> subset(vegplotid %in% myplots$vegplotid) |> clean.veg() |> fill.hts.df() |> fill.hts.df()
+
+veg <- veg |> group_by(plot) |> mutate(dom = isdominant(veg))
